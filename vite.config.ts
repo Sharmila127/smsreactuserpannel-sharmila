e350d306113ae411b11033dd3144dfe207daa6b4
Sharmila127 <sharmila6201@gmail.com>
2025-08-27 14:12:17 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
+
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/user/',   // 👈 User Panel app serve ஆகும் path (CloudFront → /user/)
+  plugins: [react(),tailwindcss()],
   build: {
-    outDir: 'dist',     // build output directory
-    assetsDir: 'assets' // static assets directory
+    outDir: 'dist',
+    assetsDir: 'assets',
   }
 })
