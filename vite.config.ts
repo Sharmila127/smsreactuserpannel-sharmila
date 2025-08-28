c0e1b86ae561261b25f76e8a4ffe28e912a1ec46
Sharmila127 <sharmila6201@gmail.com>
2025-08-28 19:58:52 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import tailwindcss from 'tailwindcss';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './', // Use './' for relative paths so it works on S3/CloudFront
-  plugins: [react(), tailwindcss()],
+  base: './', // Use relative paths for S3/CloudFront
+  plugins: [react()],
   build: {
     outDir: 'dist',       // Output folder for build
     assetsDir: 'assets',  // Folder for static assets inside dist
